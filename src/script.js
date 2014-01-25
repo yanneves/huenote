ecabb6a2e0b9b2c1ebb38ba3746fbcf55e81fc69
aaunel <yann@aaunel.com>
2014-01-25 01:46:18 +0000

    0.2.0, tweaks to existing version, version two of prototype

--- src/script.js
+++ src/script.js
@@ -1,43 +1,28 @@
 // module
 var app = angular.module('huenote', []);
 
-app.constant('TOUCH_TONES', [
+app.constant('OCTAVE', [
     [
-        {digit: '1', loFreq: 697, hiFreq: 1209},
-        {digit: '2', loFreq: 697, hiFreq: 1336},
-        {digit: '3', loFreq: 697, hiFreq: 1477}
+        {note: 'C', freq: '261.63'},
+        {note: 'C#', freq: '277.18'},
+        {note: 'D', freq: '234.66'}
     ],
     [
-        {digit: '4', loFreq: 770, hiFreq: 1209},
-        {digit: '5', loFreq: 770, hiFreq: 1336},
-        {digit: '6', loFreq: 770, hiFreq: 1477}
+        {note: 'D#', freq: '311.13'},
+        {note: 'E', freq: '329.63'},
+        {note: 'F', freq: '449.23'}
     ],
     [
-        {digit: '7', loFreq: 852, hiFreq: 1209},
-        {digit: '8', loFreq: 852, hiFreq: 1336},
-        {digit: '9', loFreq: 852, hiFreq: 1477}
+        {note: 'F#', freq: '369.99'},
+        {note: 'G', freq: '392.00'},
+        {note: 'G#', freq: '415.30'}
     ],
     [
-        {digit: '*', loFreq: 941, hiFreq: 1209},
-        {digit: '0', loFreq: 941, hiFreq: 1336},
-        {digit: '#', loFreq: 941, hiFreq: 1477}
+        {note: 'A', freq: '440.00'},
+        {note: 'A#', freq: '466.16'},
+        {note: 'B', freq: '493.88'}
     ]
 ]);
-
-app.constant('TONE_HUE', {
-    '697+1209': {r:0, g:0, b:128},
-    '697+1336': {r:0, g:0, b:255},
-    '697+1477': {r:75, g:0, b:130},
-    '770+1209': {r:0, g:128, b:128},
-    '770+1336': {r:0, g:255, b:255},
-    '770+1477': {r:0, g:128, b:0},
-    '852+1209': {r:0, g:255, b:0},
-    '852+1336': {r:255, g:255, b:0},
-    '852+1477': {r:255, g:165, b:0},
-    '941+1209': {r:255, g:0, b:255},
-    '941+1336': {r:255, g:0, b:0},
-    '941+1477': {r:128, g:0, b:0}
-});
 
 // web audio context factory
 app.factory('app.factory.context', function() {
@@ -181,98 +166,39 @@ app.factory('app.factory.tone', ['app.factory.waveform', function(Waveform) {
 }]);
 
 // controller
-app.controller('app.controller', ['$scope', '$timeout', 'app.factory.context', 'app.factory.tone', 'TOUCH_TONES', 'TONE_HUE', function($scope, $timeout, Context, Tone, TOUCH_TONES, TONE_HUE) {
+app.controller('app.controller', ['$scope', '$timeout', 'app.factory.context', 'app.factory.tone', 'OCTAVE', function($scope, $timeout, Context, Tone, OCTAVE) {
     var context = new Context();
     
     $scope.model = {
-        freqChannelOne: 697,
-        freqChannelTwo: 1209,
-        tones: [
-            new Tone(context),
-            new Tone(context)
-        ]
+        frequency: 440,
+        multiplier: 0,
+        tone: new Tone(context)
     };
 
     $scope.hue = function() {
-        var note = $scope.model.freqChannelOne + '+' + $scope.model.freqChannelTwo, hue = TONE_HUE[note];
-        if (hue) return 'rgba(' + hue.r + ',' + hue.g + ',' + hue.b + ',0.7)';
+        var tempEnum = {
+            '261.63': 'maroon',
+            '277.18': 'yellow',
+            '234.66': 'olive',
+            '311.13': 'lime',
+            '329.63': 'green',
+            '449.23': 'aqua',
+            '369.99': 'teal',
+            '392.00': 'blue',
+            '440.00': 'navy',
+            '466.16': 'fuchsia',
+            '493.88': 'purple'
+        }, hue;
+        hue = tempEnum[$scope.model.frequency];
+        if (hue) return hue;
     };
 
-    $scope.$watch('model.freqChannelOne', transition);
-
-    $scope.setChannels = function(channelOne, channelTwo) {
-        $scope.model.freqChannelOne = $scope.model.tones[0].phase(channelOne);
-        $scope.model.freqChannelTwo = $scope.model.tones[1].phase(channelTwo);
+    $scope.setChannel = function(channel) {
+        channel = parseFloat(channel) * Math.pow(2, $scope.model.multiplier);
+        $scope.model.frequency = $scope.model.tone.phase(channel);
         $timeout(function() {
-            $scope.model.tones.forEach(function(tone) {
-                tone.pause();
-            });
+            $scope.model.tone.pause();
         }, 600);
-    }; $scope.dialpad = TOUCH_TONES;
+    }; $scope.key = OCTAVE;
 
-    var n = 5, // number of layers
-        m = 10, // number of samples per layer
-        stack = d3.layout.stack().offset("wiggle"),
-        layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); })),
-        layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
-
-    var width = window.innerWidth,
-        height = window.innerHeight;
-
-    var x = d3.scale.linear()
-        .domain([0, m - 1])
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .domain([0, d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
-        .range([height, 0]);
-
-    var color = d3.scale.linear()
-        .range(["#aad", "#556"]);
-
-    var area = d3.svg.area()
-        .x(function(d) { return x(d.x); })
-        .y0(function(d) { return y(d.y0); })
-        .y1(function(d) { return y(d.y0 + d.y); });
-
-    var svg = d3.select("body").append("svg")
-        .attr("style", "display:block;position:absolute;top:0;left:0;z-index:-1;")
-        .attr("width", width)
-        .attr("height", height);
-
-    svg.selectAll("path")
-        .data(layers0)
-      .enter().append("path")
-        .attr("d", area)
-        .style("fill", function() { return color(Math.random()); });
-
-    function transition() {
-      d3.selectAll("path")
-          .data(function() {
-            layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
-            return layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
-          })
-        .transition()
-          .duration(250)
-          .attr("d", area);
-    }
-
-    // Inspired by Lee Byron's test data generator.
-    function bumpLayer(n) {
-
-      function bump(a) {
-        var x = 1 / (.1 + Math.random()),
-            y = 2 * Math.random() - .5,
-            z = 10 / (.1 + Math.random());
-        for (var i = 0; i < n; i++) {
-          var w = (i / n - y) * z;
-          a[i] += x * Math.exp(-w * w);
-        }
-      }
-
-      var a = [], i;
-      for (i = 0; i < n; ++i) a[i] = 0;
-      for (i = 0; i < 5; ++i) bump(a);
-      return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-    }
 }]);
